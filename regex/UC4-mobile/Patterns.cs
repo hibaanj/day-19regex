68f80fce96999cfe64502b85dcc620915161248e
hibaanj <anjumhiba15@gmail.com>
2022-08-02 01:33:25 +0530

    validatingmobileno

---
+++ regex/UC4-mobile/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC4_mobile
+{
+    internal class Patterns
+    {
+        public static string regexrule = "^([9][1])[ ]?([7-9][0-9]{9})$";
+        public bool Stringvalid(string lastname)
+        {
+            return Regex.IsMatch(lastname, regexrule);
+
+        }
+    }
+}
---
+++ regex/UC4-mobile/Program.cs
@@ -0,0 +1,12 @@
+﻿using UC4_mobile; 
+
+Patterns pt = new Patterns();
+if (pt.Stringvalid("919848081321"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
