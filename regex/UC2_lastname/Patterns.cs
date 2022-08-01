56b374d0317f1d732ac7b78afabe2723e3da044d
hibaanj <anjumhiba15@gmail.com>
2022-08-02 01:03:27 +0530

    checking last name

---
+++ regex/UC2_lastname/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC2_lastname
+{
+    internal class Patterns
+    {
+        public static string RegexRule = "^[A-Z][a-z]{3,}$";
+        public bool Stringvalid(string lastname)
+        {
+            return Regex.IsMatch(lastname, RegexRule);
+
+        }
+    }
+}
---
+++ regex/UC2_lastname/Program.cs
@@ -0,0 +1,12 @@
+﻿using UC2_lastname;
+
+Patterns pt = new Patterns();
+if (pt.Stringvalid("Anjum"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
