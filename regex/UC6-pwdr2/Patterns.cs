83ec937fc49398496e211eaf05213622c1a3ec49
hibaanj <anjumhiba15@gmail.com>
2022-08-02 01:50:57 +0530

    rule 2

---
+++ regex/UC6-pwdr2/Patterns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC6_pwdr2
+{
+    internal class Patterns
+    {
+        //atleast one uppercase
+        public static string regexrule = "^[a-zA-Z0-9]{8}$";
+        public bool Stringvalid(string password)
+        {
+            return Regex.IsMatch(password, regexrule);
+
+        }
+
+    }
+}
---
+++ regex/UC6-pwdr2/Program.cs
@@ -0,0 +1,13 @@
+﻿using UC6_pwdr2;
+
+
+Patterns pt = new Patterns();
+if (pt.Stringvalid("hib1267B"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
