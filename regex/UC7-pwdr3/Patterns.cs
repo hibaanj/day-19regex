68e1cc83b06c45f255678d8e6510b9197b5f0ae4
hibaanj <anjumhiba15@gmail.com>
2022-08-02 01:55:41 +0530

    rule 3 for passwordds

---
+++ regex/UC7-pwdr3/Patterns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC7_pwdr3
+{
+    internal class Patterns
+    {
+        //atleast one uppercase
+        public static string regexrule = "^[a-zA-Z0-9]{8}$";
+        public bool Stringvalid(string password)
+        {
+            return Regex.IsMatch(password, regexrule);
+
+        }
+
+    }
+}
---
+++ regex/UC7-pwdr3/Program.cs
@@ -0,0 +1,13 @@
+﻿using UC7_pwdr3;
+
+
+Patterns pt = new Patterns();
+if (pt.Stringvalid("hib1267B"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
