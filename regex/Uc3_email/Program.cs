17afe3883fde5cbd2873a8105d9d4cf1e2f9acde
hibaanj <anjumhiba15@gmail.com>
2022-08-02 01:11:18 +0530

    email validation

---
+++ regex/Uc3_email/Program.cs
@@ -0,0 +1,12 @@
+﻿using Uc3_email;
+
+Pattern pt = new Pattern();
+if (pt.Stringvalid("anjumhiba15@gmail.com"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
