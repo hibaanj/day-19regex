eab95e27749d78db45f8b52ca3d42b5834790be6
hibaanj <anjumhiba15@gmail.com>
2022-08-02 01:40:57 +0530

    rule 1 for passwords

---
+++ regex/UC5_pwdrule1/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC5_pwdrule1
+{
+    internal class Pattern
+    {
+        public static string regexrule = "^[a-zA-Z0-9]{8}$";
+        public bool Stringvalid(string password)
+        {
+            return Regex.IsMatch(password, regexrule);
+
+        }
+
+    }
+}
---
+++ regex/UC5_pwdrule1/Program.cs
@@ -0,0 +1,13 @@
+﻿using UC5_pwdrule1;
+
+
+Pattern pt = new Pattern();
+if (pt.Stringvalid("hib12b67"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
