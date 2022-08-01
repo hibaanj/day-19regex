388aa063c7436fcd75173c08ad97655d070c899f
hibaanj <anjumhiba15@gmail.com>
2022-08-02 02:15:46 +0530

    email use cases

---
+++ regex/UC9-emails/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Uc9_email
+{
+    internal class Pattern
+    {
+        public static string RegexRule = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        public bool Stringvalid(string lastname)
+        {
+            return Regex.IsMatch(lastname, RegexRule);
+
+        }
+    }
+}
---
+++ regex/UC9-emails/Program.cs
@@ -0,0 +1,12 @@
+﻿using Uc9_email;
+
+Pattern pt = new Pattern();
+if (pt.Stringvalid("anjumhiba15@gmail.com.in"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
