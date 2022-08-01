c1462cf5e97eba69c9bd9653ab13304fdf743c8c
hibaanj <anjumhiba15@gmail.com>
2022-08-02 02:12:36 +0530

    rule4

---
+++ regex/UC8-pwd4/Patterns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC8_pwd4
+{
+    internal class Patterns
+    {
+        //atleast one uppercase
+        public static string regexrule = "^[a-zA-Z0-9]{7}[./#$*&]$";
+        public bool Stringvalid(string password)
+        {
+            return Regex.IsMatch(password, regexrule);
+
+        }
+
+    }
+}
---
+++ regex/UC8-pwd4/Program.cs
@@ -0,0 +1,13 @@
+﻿using UC8_pwd4;
+
+
+Patterns pt = new Patterns();
+if (pt.Stringvalid("hibB678$"))
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+Console.ReadKey();
