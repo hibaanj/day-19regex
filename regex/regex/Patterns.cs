28a81e4751fbaf3659c0b4082fca07b5bdd8b1b8
hibaanj <anjumhiba15@gmail.com>
2022-08-02 00:56:36 +0530

    UC1-first name

---
+++ regex/regex/Patterns.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace regex
+{
+    class Patterns
+    {
+        public static string RegexRules = "^[A-Z][a-z]{3,}$";
+        //^[1-9][0-9]{2}[ ]?[0-9]{3}$
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+    //name-^[A-Z][a-z]{3,}$
+    
+
+
+}
---
+++ regex/regex/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using regex;
+
+Patterns patterns = new Patterns();
+//Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
+if (patterns.validateString("Hiba"))
+{
+    Console.WriteLine("Valid");
+}
+else
+    Console.WriteLine("Invalid");
+Console.ReadKey();
